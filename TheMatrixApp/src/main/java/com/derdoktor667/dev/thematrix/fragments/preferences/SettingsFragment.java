520d0b6f173e899ded7a5fca9246497cb4f879d5
Wastl Kraus <derdoktor667@gmail.com>
2013-10-06 20:28:00 +0200

    ...prepare new release

--- TheMatrixApp/src/main/java/com/derdoktor667/dev/thematrix/fragments/preferences/SettingsFragment.java
+++ TheMatrixApp/src/main/java/com/derdoktor667/dev/thematrix/fragments/preferences/SettingsFragment.java
@@ -1,16 +1,16 @@
 package com.derdoktor667.dev.thematrix.fragments.preferences;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.derdoktor667.dev.thematrix.R;
 
-public class SettingsFragment extends PreferenceFragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class SettingsFragment extends Fragment {
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container, false);
     }
 }
