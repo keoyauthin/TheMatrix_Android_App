fb951d6035195cec2b92ebfc89a6e1dbab7e57b5
Wastl Kraus <derdoktor667@gmail.com>
2013-10-26 06:41:11 +0200

    ....test AS in Win7 64bit

---
+++ TheMatrixApp/src/main/java/com/derdoktor667/dev/thematrix/utils/Utils.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2013 Wastl Kraus <derdoktor667@gmail.com>
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.derdoktor667.dev.thematrix.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Utils {
+
+    /**
+     * Checks if Android is online
+     * 
+     * @param context
+     * @return returns true if online
+     */
+    public static boolean isAndroidOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+}
